dade28619125b44c8f3e7299609e63414b3b0409
unknown <penkov.dimit@gmail.com>
2018-10-15 12:59:48 +0300

    triagalnik

--- triagalnik_everything.cpp
+++ triagalnik_everything.cpp
@@ -3,7 +3,42 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    double Ax, Ay, Bx, By, Cx, Cy;
 
-return 0;
+    cout<<"Vuvedete koordinatite na vyrhovete na triugulnika:\n";
+    cin>>Ax>>Ay>>Bx>>By>>Cx>>Cy;
+
+    double a, b, c;
+
+    a = sqrt(pow(Bx - Cx, 2) + pow(By - Cy, 2));
+    b = sqrt(pow(Ax - Cx, 2) + pow(Ay - Cy, 2));
+    c = sqrt(pow(Ax - Bx, 2) + pow(Ay - By, 2));
+
+
+    cout<<"a = "<<a<<"\nb = "<<b<<"\nc = "<<c<<endl;
+
+    if(a + b <= c || a + c <= b || b + c <= a)
+    {
+        cout<<"Nqma takuv triagalnik!\n";
+        return 0;
+    }
+
+    double ma, mb, mc;
+
+    ma = sqrt(2*c*c + 2*b*b - a*a)/2;
+    mb = sqrt(2*a*a + 2*c*c - b*b)/2;
+    mc = sqrt(2*a*a + 2*b*b - c*c)/2;
+
+    cout<<"ma = "<<ma<<"\nmb = "<<mb<<"\nmc = "<<mc<<endl;
+
+    double la, lb, lc;
+
+    la = sqrt(b*c - b*c*a*a/pow(b+c, 2));
+    lb = sqrt(a*c - a*c*b*b/pow(a+c, 2));
+    lc = sqrt(a*b - a*b*c*c/pow(a+b, 2));
+
+    cout<<"la = "<<la<<"\nlb = "<<lb<<"\nlc = "<<lc<<endl;
+    return 0;
 }
