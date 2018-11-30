62a492d4aecc14171f504165edbc9ed09ee20db2
Dimitko <penkov.dimit@gmail.com>
2018-11-30 20:29:22 +0200

    Updated strcmp.cpp to work when the second string is longer than the first but is the same as the first in the beginning.

--- Exercises/Exercise 8/strcmp.cpp
+++ Exercises/Exercise 8/strcmp.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
-int main()
-{
-    char str[20] = "Hello!";
-    char str2[20] = "Hello!";
-    int  i = 0;
+
+int main(){
+    char str[50] = "Hello world!";
+    char str2[50] = "Hello world!2";
+    int i;
     bool flag = false;
-    for(i = 0; str[i] && !flag; i++)
-    {
-        if(str[i] != str2[i])
-        {
+    for(i = 0; str[i] && str2[i] && !flag; i++){
+        if(str[i] != str2[i]){
             cout<<str[i] - str2[i]<<endl;
             flag = true;
         }
     }
-
+    if(str[i]!=str2[i] && !flag){
+        cout<<str[i]-str2[i]<<endl;
+        flag = true;
+    }
     if(!flag){
         cout<<0<<endl;
     }
-
-
     return 0;
 }
+
